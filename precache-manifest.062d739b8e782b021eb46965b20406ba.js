7a5c8103e83b6993fc07bb806867ddb80ef1b840
jiun-dev <58166091+jiun-dev@users.noreply.github.com>
2020-02-25 19:27:22 +0900

    Updates

---
+++ precache-manifest.062d739b8e782b021eb46965b20406ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ee283da09dea41aca35b18d396ce20",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0e6e4241f63bd4d80bb8",
+    "url": "/movie_app_2020/static/css/main.bad54cf0.chunk.css"
+  },
+  {
+    "revision": "90463dbad7e958367839",
+    "url": "/movie_app_2020/static/js/2.7cd7ac10.chunk.js"
+  },
+  {
+    "revision": "0e6e4241f63bd4d80bb8",
+    "url": "/movie_app_2020/static/js/main.7d57f7a6.chunk.js"
+  },
+  {
+    "revision": "114d3434d006dc29083b",
+    "url": "/movie_app_2020/static/js/runtime~main.6c3fa8d1.js"
+  }
+]);
